86bd83e4f63c0f2bdebaba5c0016e3dca815462a
Kravtsov Roman <kravroma@us.ibm.com>
2018-02-05 15:38:44 +0200

    Remove import statement before Specification and semicolons

--- chapter1/src/test/groovy/com/manning/spock/chapter1/BetterSpec.groovy
+++ chapter1/src/test/groovy/com/manning/spock/chapter1/BetterSpec.groovy
@@ -1,17 +1,17 @@
 package com.manning.spock.chapter1
 
-import com.manning.spock.chapter1.billing.Client;
-import com.manning.spock.chapter1.billing.CreditCardBilling;
+import com.manning.spock.chapter1.billing.Client
+import com.manning.spock.chapter1.billing.CreditCardBilling
 
 import spock.lang.*
 
 
-class BetterSpec extends spock.lang.Specification{
+class BetterSpec extends Specification{
 	def "Client should have a bonus if he spends more than 100 dollars"() {
 		when: "a client buys something with value at least 100"
-		def client = new Client();
-		def billing = new CreditCardBilling();
-		billing.charge(client,150);
+		def client = new Client()
+		def billing = new CreditCardBilling()
+		billing.charge(client,150)
 
 		then: "Client should have the bonus option active"
 		client.hasBonus() == true
@@ -19,10 +19,10 @@ class BetterSpec extends spock.lang.Specification{
 	
 	def "Client loses bones is he does not accept the transaction"() {
 		when: "a client buys something and later changes mind"
-		def client = new Client();
-		def billing = new CreditCardBilling();
-		billing.charge(client,150);
-		client.rejectsCharge();
+		def client = new Client()
+		def billing = new CreditCardBilling()
+		billing.charge(client,150)
+		client.rejectsCharge()
 
 		then: "Client should have the bonus option inactive"
 		client.hasBonus() == false 
